a7707acd9dde892f272a1a5b94762ffa7ef6c5fe
crc2017 <ruiz_cesar08@hotmail.com>
2018-01-19 18:28:58 +0100

    (FIX:DESIGNMODAL) style and design

---
+++ src/app/components/dialog-premios/dialog-premios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-premios',
+  templateUrl: './dialog-premios.component.html',
+  styleUrls: ['./dialog-premios.component.css']
+})
+export class DialogPremiosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
